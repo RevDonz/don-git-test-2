eac5840c60fd7c666fc007f98fd0f6205cd7dfa8
RevDonz <aprilio842@gmail.com>
2020-06-05 20:40:07 +0700

    Mengubah file index.php

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
 </head>
 <body>
     <h1>Hello Wolrd!</h1>
-    <p?>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, tenetur, sint nemo maxime obcaecati corrupti animi voluptatum beatae architecto repudiandae dolore hic iste ab eos quisquam expedita dicta quos nisi!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias est commodi harum perspiciatis, ab laboriosam dicta obcaecati explicabo unde nihil dignissimos necessitatibus, rerum maxime perferendis a nam omnis cumque placeat?</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 </html>
